d966ae23102b9362cbc5c2415a9553f4b5778877
tamada007 <tamada007@hotmail.com>
2019-07-24 12:20:42 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -34,15 +34,15 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: 'cfworker',
+      hidden: false,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+        //'node-cfworker.etherdream.com': 6,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'lively-silence-e817.tamada007.workers.dev': 8,
+        'lively-silence-e817.tamada007.workers.dev': 6,
         'a.007.workers.dev': 1,
         'a.hehe.workers.dev': 1,
         'a.lulu.workers.dev': 1,
