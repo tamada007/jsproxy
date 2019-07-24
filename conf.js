30a964ac0959482c066bf8062fee8139e417d80b
tamada007 <tamada007@hotmail.com>
2019-07-24 13:24:19 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -37,15 +37,15 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+//         'node-cfworker.etherdream.com': 6,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'lively-silence-e817.tamada007.workers.dev': 1,
-        'a.007.workers.dev': 1,
-        'a.hehe.workers.dev': 1,
-        'a.lulu.workers.dev': 1,
-        'shrill-unit-8594.jsproxy.workers.dev': 1,
+        'tome.tamada007.workers.dev': 1,
+//         'a.007.workers.dev': 1,
+//         'a.hehe.workers.dev': 1,
+//         'a.lulu.workers.dev': 1,
+//         'shrill-unit-8594.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -53,8 +53,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
-//   node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+//   node_default: 'mysite',
+  node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
