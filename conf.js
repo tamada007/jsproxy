e40b93f0347d8d88b12e92150378019a854c78d7
tamada007 <tamada007@hotmail.com>
2019-07-24 15:26:59 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -37,11 +37,11 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 6,
+//         'node-cfworker.etherdream.com': 6,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'tome.tamada007.workers.dev': 1,
+        'tome.tamada007.workers.dev': 6,
         'a.007.workers.dev': 1,
         'a.hehe.workers.dev': 1,
         'a.lulu.workers.dev': 1,
@@ -53,8 +53,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-//   node_default: 'mysite',
-  node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite',
+//   node_default: /jsproxy\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -65,10 +65,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+//   assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+  assets_cdn: 'assets/',
 
   /**
    * 自定义注入页面的 HTML
